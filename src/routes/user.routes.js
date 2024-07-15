7c3b225ee9121fe6b44a55fafbb0d823386a4ed6
Arka Das <arkadas20102001@gmail.com>
2024-07-15 12:17:33 +0530

    login and logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { upload } from "../middlewares/multer.middleware.js";
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router=Router()
 router.route("/register").post(
@@ -16,6 +17,14 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT,loginUser)
+
+
+
 export default router
 
 
